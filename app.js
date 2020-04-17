dd01503740a23c87c178b79ecd69779543064836
andreyaksenov <andrey.aksenov@jetbrains.com>
2020-04-17 13:20:29 +0300

    Rewrite EasyHttp with Promises

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
-const http = new easyHttp();
+const http = new EasyHTTP();
 
-http.get('https://jsonplaceholder.typicode.com/posts', function (error, response) {
-    if(error) {
-        console.log(error);
-    } else {
-        console.log(response);
-    }
-});
+// http.get('https://jsonplaceholder.typicode.com/users')
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error));
 
 const data = {
-    title: 'Custom post',
-    body: 'This is a custom post'
-};
+    name: 'Andrew',
+    username: 'duuusha',
+    email: 'a@a.com'
+}
+
+// http.post('https://jsonplaceholder.typicode.com/users', data)
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error));
+
+// http.put('https://jsonplaceholder.typicode.com/users/2', data)
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error));
+
+http.delete('https://jsonplaceholder.typicode.com/users/2')
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+
+
+
+
+// const http = new easyHttp();
+//
+// http.get('https://jsonplaceholder.typicode.com/posts', function (error, response) {
+//     if(error) {
+//         console.log(error);
+//     } else {
+//         console.log(response);
+//     }
+// });
+//
+// const data = {
+//     title: 'Custom post',
+//     body: 'This is a custom post'
+// };
 
 // http.post('https://jsonplaceholder.typicode.com/posts', data, function(error, post) {
 //     if(error) {
